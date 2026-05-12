8b928f48dfca131f0a7e7dcde9bc05bb918d4b25
Kafka Gifassya <62605209+KafkaPearce@users.noreply.github.com>
2026-05-12 22:05:36 +0700

    Menu Jadi cok

--- main.go
+++ main.go
@@ -20,11 +20,11 @@ func cls() {
 
 // Fungsi header untuk menampilkan header pada aplikasi
 func header() {
-	// Color codes untuk terminal
-	green := "\033[92m"
-	lightGreen := "\033[32m"
-	reset := "\033[0m"
-	bold := "\033[1m"
+	var green, lightGreen, reset, bold string
+	green = "\033[92m"
+	lightGreen = "\033[32m"
+	reset = "\033[0m"
+	bold = "\033[1m"
 
 	// Header dengan dekorasi
 	fmt.Printf("%s", green)
@@ -42,8 +42,26 @@ func header() {
 	fmt.Println()
 }
 
-// Fungsi main
-func main() {
+func menu(selectOption *int, cnt *int) {
 	cls()
 	header()
+	if (*selectOption < 1 || *cnt > 2) && *cnt != 0 {
+		fmt.Println("Pilihan tidak valid. Silakan pilih menu yang sesuai.")
+	} else {
+		fmt.Println()
+	}
+	fmt.Printf("%-10s[1] 🔧 Manage Component\n", "")
+	fmt.Printf("%-10s[2] ↩️ Exit app\n", "")
+	fmt.Println()
+	fmt.Printf("%-10s╰┈➤ ", "")
+	fmt.Scan(selectOption)
+	*cnt++
+}
+
+// Fungsi main
+func main() {
+	var selectOption int = 1
+	var cnt int = 0
+
+	menu(&selectOption, &cnt)
 }
